556084ed1e1b5f88a80bd7e1dc94e331262bf611
Maikel <Maikel@DESKTOP-LNQGT8H>
2021-12-10 00:38:40 -0200

    Adicionar arquivos de projeto.

---
+++ Primeiro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello";
+            var b = "World"; // aperta f9 pra criar um breakpoint numa linha
+            Console.WriteLine(a+ b ); //aperta f10 pra continuar rodando linha por linha ou f5 pra ir pro final do codigo
+        }
+    }
+}
